1c867d88875bb11b8bdaec16cb3e4ab7d87e2fee
Daniel L. Buchko <dbuchko@pivotal.io>
2016-08-26 13:21:56 -0400

    Empty project to validate SCDF dependencies can be pulled.

---
+++ src/main/java/io/pivotal/cloud/ScdfValidatorApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdfValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdfValidatorApplication.class, args);
+	}
+}
